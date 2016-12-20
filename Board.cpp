31c4f80feed9e9d88c27998d8ad9eb2244ad86ea
PapyRedstone <febvrealex@yahoo.fr>
2016-12-20 17:08:44 +0100

    auto added to github

--- Board.cpp
+++ Board.cpp
@@ -3,3 +3,7 @@
 Board::Board(){}
 
 Board::~Board(){}
+
+std::shared_ptr<Piece> Board::getPiece(int x, int y){
+	return mBoard[x][y].mPiece;
+}
--- Board.hpp
+++ Board.hpp
@@ -8,16 +8,14 @@
 struct Case{
 	Color mColor;
 	std::shared_ptr<Piece> mPiece;
-}
+};
 
 class Board{
 public:
 	Board();
 	~Board();
 
-	std::shared_ptr<Piece> getPiece(int x, int y){
-		return mBoard[x][y].mPiece;
-	}
+	std::shared_ptr<Piece> getPiece(int x, int y);
 
 private:
 	std::array<std::array<Case,8>,8> mBoard;
